50f077c553c30e724de52e85c2846b9925ff16a0
Nikki <npeel3497@gmail.com>
2020-10-21 13:43:45 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+  },
+
+  variants: {},
+  plugins: [],
+}
